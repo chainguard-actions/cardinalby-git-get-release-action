6b05bc706bb26d7646ada5c88379ede140b8036c
actionchain <actionchain@chainguard.dev>
2026-05-12 21:02:21 +0000

    Update cardinalby/git-get-release-action@1.1.1

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# git-get-release-action
+![Build Status](https://github.com/cardinalby/git-get-release-action/workflows/build-test/badge.svg)
+
+# Get GitHub release information 
+
+Allows you to get release information by: release id, tag, commit SHA (current commit or specified).
+
+If release not found finishes with error.
+
+## Inputs
+
+Specify 1 input from the list to search release by:
+
+* `releaseId` Release Id (number).
+* `tag` Tag name.
+* `commitSha` SHA of commit. Can be used to find a draft release.
+* `releaseName` Release name. Returns most recent found. Can be used to find a draft release.
+* `latest` Set `1` or `true` to get the latest non-draft release.
+
+If no inputs specified, the action will try to get release for the current commit SHA.
+
+* `searchLimit` Optional, default: `90`. If you use `commitSha` or `releaseName` inputs you can 
+also specify how many releases action should retrieve to perform a search. Specify more than 90 if
+you age going to search for old releases and less if you want to speed up the search.
+
+## Env variable
+
+You should set `GITHUB_TOKEN` env variable to enable action to access GitHub API. See example.
+
+## Outputs
+Values from [API](https://docs.github.com/en/rest/reference/repos#releases) response object:
+
+* `html_url`
+* `upload_url` can be used to upload assets
+* `tarball_url`
+* `zipball_url`
+* `id`
+* `tag_name`
+* `target_commitish`
+* `name`
+* `body`
+* `draft` contains `true` or `false` string value
+* `prerelease` contains `true` or `false` string value
+* `created_at`
+* `published_at`
+
+## Example usage()
+```yaml
+- uses: cardinalby/git-get-release-action@v1.1
+  env:
+    GITHUB_TOKEN: ${{ github.token }}
+  with:
+    tag: '1.2.3'    
+```
+
+## Privacy
+
+This Action contacts Chainguard's licensing server to verify authorization. Connection metadata (IP address, GitHub repository identifier, timestamp, and any metadata encoded in the auth token) is transmitted to Chainguard, Inc. even if authorization is denied in accordance with our [Privacy Notice](https://www.chainguard.dev/legal/privacy-notice)
---
+++ src/main.ts
@@ -0,0 +1,123 @@
+import * as ghActions from '@actions/core';
+import { actionInputs } from './actionInputs';
+import { actionOutputs } from './actionOutputs';
+import { getOctokit, context } from "@actions/github";
+import { GitHub } from '@actions/github/lib/utils';
+import { components } from '@octokit/openapi-types';
+
+type GithubApi = InstanceType<typeof GitHub>;
+type ReleaseResponse = components['schemas']['release'];
+
+// noinspection JSUnusedLocalSymbols
+async function run(): Promise<void> {
+    try {
+        await runImpl();
+    } catch (error) {
+        ghActions.setFailed(error.message);
+    }
+}
+
+async function runImpl() {
+    if (process.env.GITHUB_TOKEN === undefined) {
+        throw new Error('GITHUB_TOKEN env variable is not set');
+    }
+    const github = getOctokit(process.env.GITHUB_TOKEN);
+    const { owner, repo } = context.repo;
+
+    let releaseResponse: ReleaseResponse;
+    if (actionInputs.releaseId) {
+        ghActions.info(`Retrieving release with id = ${actionInputs.releaseId}...`);
+        releaseResponse = (await github.repos.getRelease({
+            owner,
+            repo,
+            release_id: actionInputs.releaseId
+        })).data;
+    } else if (actionInputs.tag) {
+        ghActions.info(`Retrieving release by ${actionInputs.tag} tag...`);
+        releaseResponse = (await github.repos.getReleaseByTag({
+            owner,
+            repo,
+            tag: actionInputs.tag
+        })).data;
+    } else if (actionInputs.commitSha) {
+        ghActions.info(`Retrieving release by ${actionInputs.commitSha} commitish...`);
+        releaseResponse = await findReleaseByCommitSha(github, owner, repo, actionInputs.commitSha);
+    } else if (actionInputs.releaseName) {
+        ghActions.info(`Retrieving release by ${actionInputs.releaseName} name...`);
+        releaseResponse = await findRelease(github, owner, repo, release => release.name === actionInputs.releaseName);
+    } else if (actionInputs.latest) {
+        ghActions.info(`Retrieving the latest release...`);
+        releaseResponse = (await github.repos.getLatestRelease({
+            owner,
+            repo
+        })).data;
+    } else if (context.sha) {
+        ghActions.info(`Retrieving release for current commit ${context.sha}...`);
+        releaseResponse = await findReleaseByCommitSha(github, owner, repo, context.sha);
+    } else {
+        throw new Error('No inputs set, context.sha is empty');
+    }
+
+    ghActions.info('Release found:');
+    console.log(releaseResponse);
+
+    setOutputs(releaseResponse);
+}
+
+function setOutputs(release: ReleaseResponse)
+{
+    actionOutputs.html_url.setValue(release.html_url);
+    actionOutputs.upload_url.setValue(release.upload_url);
+    actionOutputs.tarball_url.setValue(release.tarball_url || '');
+    actionOutputs.zipball_url.setValue(release.zipball_url || '');
+    actionOutputs.id.setValue(release.id);
+    actionOutputs.tag_name.setValue(release.tag_name);
+    actionOutputs.target_commitish.setValue(release.target_commitish);
+    actionOutputs.name.setValue(release.name|| '');
+    actionOutputs.body.setValue(release.body|| '');
+    actionOutputs.draft.setValue(release.draft);
+    actionOutputs.prerelease.setValue(release.prerelease);
+    actionOutputs.created_at.setValue(release.created_at);
+    actionOutputs.published_at.setValue(release.published_at || '');
+}
+
+async function findReleaseByCommitSha(github: GithubApi, owner: string, repo: string, sha: string) {
+    return findRelease(github, repo, owner, release => release.target_commitish === sha);
+}
+
+async function findRelease(
+    github: GithubApi,
+    owner: string,
+    repo: string,
+    predicate: (release: ReleaseResponse) => boolean
+) {
+    const releases = fetchReleases(github, owner, repo);
+    for await (const release of releases) {
+        if (predicate(release)) {
+            return release;
+        }
+    }
+    throw new Error('Release not found');
+}
+
+async function* fetchReleases(github: GithubApi, owner: string, repo: string): AsyncGenerator<ReleaseResponse> {
+    let page = 1;
+    let i = 0;
+    while (true) {
+        const releases = (await github.repos.listReleases({ owner, repo, per_page: 30, page})).data;
+        if (releases.length === 0) {
+            return;
+        }
+        for (const release of releases) {
+            yield release;
+            ++i;
+            if (i >= actionInputs.searchLimit) {
+                return;
+            }
+        }
+        ++page;
+    }
+}
+
+// noinspection JSIgnoredPromiseFromCall
+run();
